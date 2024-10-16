d5e2eb7c1b6ab4824e45f202d1cb82605750ba67
Ny Huynh <129814849+ny-huynh@users.noreply.github.com>
2024-10-16 10:21:38 +0700

    feat: MARP-1201 improve Jira connector (#25)
    * feat: MARP-1201 improve Jira connector
    
    * feat: MARP-1201 add demo UI
    
    * feat: add query params for delete API
    
    * refactor: remove unused code

---
+++ jira-connector/src/com/axonivy/connector/jira/models/SearchIssue.java
@@ -0,0 +1,92 @@
+package com.axonivy.connector.jira.models;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class SearchIssue {
+
+	@JsonProperty("expand")
+	@JsonInclude(Include.NON_NULL)
+	private List<String> expand;
+
+	@JsonProperty("fields")
+	@JsonInclude(Include.NON_NULL)
+	private List<String> fields;
+
+	@JsonProperty("fieldsByKeys")
+	private boolean fieldsByKeys;
+
+	@JsonProperty("jql")
+	@JsonInclude(Include.NON_NULL)
+	private String jql;
+
+	@JsonProperty("maxResults")
+	private int maxResults;
+
+	@JsonProperty("startAt")
+	private int startAt;
+
+	@JsonProperty("validateQuery")
+	@JsonInclude(Include.NON_NULL)
+	private String validateQuery;
+
+	public List<String> getExpand() {
+		return expand;
+	}
+
+	public void setExpand(List<String> expand) {
+		this.expand = expand;
+	}
+
+	public List<String> getFields() {
+		return fields;
+	}
+
+	public void setFields(List<String> fields) {
+		this.fields = fields;
+	}
+
+	public boolean isFieldsByKeys() {
+		return fieldsByKeys;
+	}
+
+	public void setFieldsByKeys(boolean fieldsByKeys) {
+		this.fieldsByKeys = fieldsByKeys;
+	}
+
+	public String getJql() {
+		return jql;
+	}
+
+	public void setJql(String jql) {
+		this.jql = jql;
+	}
+
+	public int getMaxResults() {
+		return maxResults;
+	}
+
+	public void setMaxResults(int maxResults) {
+		this.maxResults = maxResults;
+	}
+
+	public int getStartAt() {
+		return startAt;
+	}
+
+	public void setStartAt(int startAt) {
+		this.startAt = startAt;
+	}
+
+	public String getValidateQuery() {
+		return validateQuery;
+	}
+
+	public void setValidateQuery(String validateQuery) {
+		this.validateQuery = validateQuery;
+	}
+
+}
