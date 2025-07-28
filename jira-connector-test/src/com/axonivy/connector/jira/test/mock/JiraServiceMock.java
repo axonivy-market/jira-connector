e51361db258dd721ed83e95814bb6020326899cd
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2025-07-28 14:59:57 +0700

    MARP-1983 end 2 end adapt all connectors on market place for new multi environment testing (#48)

--- jira-connector-test/src/com/axonivy/connector/jira/test/mock/JiraServiceMock.java
+++ jira-connector-test/src/com/axonivy/connector/jira/test/mock/JiraServiceMock.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import javax.annotation.security.PermitAll;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,18 +16,62 @@ import javax.ws.rs.core.Response;
 
 import io.swagger.v3.oas.annotations.Hidden;
 
-@Path("jira")
+@Path("jiraMock")
 @PermitAll
 @Hidden
 public class JiraServiceMock {
+
+  @POST
+  @Path("issue/{key}/comment")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createComment(@PathParam(value = "key") String issueKey) {
+    return Response.status(201).entity(load("createComment.json")).build();
+  }
+
+  @POST
+  @Path("issue")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createIssue() {
+    return Response.status(201).entity(load("createIssue.json")).build();
+  }
 
   @GET
   @Path("issue/{issueKey}")
   @Produces(MediaType.APPLICATION_JSON)
   public Response readIssue(@PathParam("issueKey") String issueKey) {
-    return Response.status(200)
-            .entity(load("issue.json"))
-            .build();
+    return Response.status(200).entity(load("issue.json")).build();
+  }
+
+  @POST
+  @Path("issue/{key}/worklog")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createWorklog(@PathParam(value = "key") String issueKey) {
+    return Response.status(201).entity(load("createWorklog.json")).build();
+  }
+
+  @DELETE
+  @Path("issue/{key}/worklog/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createWorklog(@PathParam(value = "key") String issueKey, @PathParam(value = "id") String workLogId) {
+    return Response.noContent().build();
+  }
+
+  @GET
+  @Path("/issue/{key}/worklog")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getIssueWorkLog(@PathParam(value = "key") String issueKey,
+      @PathParam(value = "id") String workLogId) {
+    return Response.status(200).entity(load("getWorklog.json")).build();
+  }
+
+  @GET
+  @Path("project/search")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response projectSearch() {
+    return Response.status(200).entity(load("project.json")).build();
   }
 
   private static String load(String path) {
---
+++ jira-connector-test/src_test/com/axonivy/connector/jira/test/context/MultiEnvironmentContextProvider.java
@@ -0,0 +1,60 @@
+package com.axonivy.connector.jira.test.context;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider;
+
+import com.axonivy.connector.jira.test.constant.TestEnvironmentConstants;
+
+public class MultiEnvironmentContextProvider implements TestTemplateInvocationContextProvider {
+	@Override
+	public boolean supportsTestTemplate(ExtensionContext context) {
+		return true;
+	}
+
+	@Override
+	public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(ExtensionContext context) {
+		return Stream.of(
+				new TestEnironmentInvocationContext(TestEnvironmentConstants.REAL_CALL_CONTEXT_DISPLAY_NAME),
+				new TestEnironmentInvocationContext(TestEnvironmentConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+	}
+
+	private class TestEnironmentInvocationContext implements TestTemplateInvocationContext {
+		private String contextDisplayName;
+
+		private TestEnironmentInvocationContext(String contextDisplayName) {
+			this.contextDisplayName = contextDisplayName;
+		}
+
+		@Override
+		public String getDisplayName(int invocationIndex) {
+			return contextDisplayName;
+		}
+
+		@Override
+		public List<Extension> getAdditionalExtensions() {
+			return Collections.singletonList(new ParameterResolver() {
+
+				@Override
+				public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+						throws ParameterResolutionException {
+					return extensionContext;
+				}
+
+				@Override
+				public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+						throws ParameterResolutionException {
+					return ExtensionContext.class == parameterContext.getParameter().getType();
+				}
+			});
+		}
+	}
+}
