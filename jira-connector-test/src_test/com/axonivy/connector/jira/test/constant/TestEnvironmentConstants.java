4f28ea7f423e465739fec8b4e8d81079d3cc2a6f
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2025-08-22 16:31:31 +0700

    Merge pull request #55 from axonivy-market/feature/MARP-2631-Unify-code-for-e2e-testing-approach
    MARP-2631 Remove unused codes

--- jira-connector-test/src_test/com/axonivy/connector/jira/test/constant/TestEnvironmentConstants.java
+++ jira-connector-test/src_test/com/axonivy/connector/jira/test/constant/TestEnvironmentConstants.java
@@ -1,11 +1,7 @@
 package com.axonivy.connector.jira.test.constant;
 
 public class TestEnvironmentConstants {
-  public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
-  public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
   public static final String URL_PROPERTY_NAME = "url";
   public static final String USERNAME_PROPERTY_NAME = "username";
   public static final String PASSWORD_PROPERTY_NAME = "password";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
