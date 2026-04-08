9f135aca3817a6fd4329cd578c1c70e17f5322b3
AAVN\hdvu <vu.hodac@axonactive.com>
2026-04-08 16:57:53 +0700

    TA-188 refactor util

--- jira-connector/src/com/axonivy/connector/jira/utils/JsonUtils.java
+++ jira-connector/src/com/axonivy/connector/jira/utils/JsonUtils.java
@@ -1,19 +1,21 @@
 package com.axonivy.connector.jira.utils;
 
 import java.util.Map;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ch.ivyteam.ivy.environment.Ivy;
-
 public class JsonUtils {
-
+	
+	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+	
 	public static String convertMapToJsonString(Map<String, Object> map) {
-		ObjectMapper mapper = new ObjectMapper();
 		String jsonBody = null;
 		try {
-			jsonBody = mapper.writeValueAsString(map);
+			jsonBody = OBJECT_MAPPER.writeValueAsString(map);
 		} catch (JsonProcessingException e) {
-			Ivy.log().error(e);
+			Ivy.log().error("Failed to serialize map to JSON string", e);
 		}
 		return jsonBody;
 	}
