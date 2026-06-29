f52dd01ec0c810d162df386ee02bf6e8b04f6f32
quanpham-axonivy <quan.phamvan@axonactive.com>
2026-06-29 08:50:59 +0700

    format

--- jira-connector/src/com/axonivy/connector/jira/models/Issuetype.java
+++ jira-connector/src/com/axonivy/connector/jira/models/Issuetype.java
@@ -2,13 +2,13 @@ package com.axonivy.connector.jira.models;
 
 public class Issuetype {
 
-  private String id;
+	private String id;
 
 	public String getId() {
-    return id;
-  }
+		return id;
+	}
 
-  public void setId(String id) {
-    this.id = id;
-  }
+	public void setId(String id) {
+		this.id = id;
+	}
 }
